968ffb325067055e0e65f380fae0a7b794657dee
yuanhaohua <yuanhaohua123@qq.com>
2016-05-16 19:08:34 +0800

    yuan+ 创建工程

---
+++ Volunteers/Volunteers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Volunteers
+//
+//  Created by yuan on 16/5/16.
+//  Copyright © 2016年 支援者. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Volunteers/Volunteers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Volunteers
+//
+//  Created by yuan on 16/5/16.
+//  Copyright © 2016年 支援者. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
